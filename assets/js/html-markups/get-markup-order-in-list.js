9ec1c49e4cade70042068f0b31780172791f75fd
Vitali <vskrabatun@gmail.com>
2020-04-26 13:41:27 +0300

    refactor: for new Database

--- assets/js/html-markups/get-markup-order-in-list.js
+++ assets/js/html-markups/get-markup-order-in-list.js
@@ -3,14 +3,29 @@ export function getMarkupOrderInList(order) {
   <div class="order-list__item" id="${order.id}">
     <div class="order-list__item-row">
         <h4 class="order-list__item-order">Order <span>${order.id}</span></h4>
-        <h3 class="order-list__item-ordered-date">${order.summary.createdAt}</h3>
+        <h3 class="order-list__item-ordered-date">${order.createdAt}</h3>
     </div>
     <div class="order-list__item-row">
-        <h5 class="order-list__item-customer">${order.customerInfo.firstName} ${order.customerInfo.lastName}</h5>
-        <h5 class="order-list__item-order-time order-list__item-order-time--in-time">${order.summary.status}</h5>
+        <h5 class="order-list__item-customer">${order.firstNameCustomer} ${order.lastNameCustomer}</h5>
+        <h5 class="order-list__item-order-time order-list__item-order-time--in-time">${order.status}</h5>
     </div>
     <div class="order-list__item-row">
-        <h5 class="order-list__item-shipped">Shipped: <span>${order.summary.shippedAt}</span></h5>
+        <h5 class="order-list__item-shipped">Shipped: <span>${order.shippedAt}</span></h5>
     </div>
-  </div>`;    
+  </div>`; 
+
+  // return `
+  // <div class="order-list__item" id="${order.id}">
+  //   <div class="order-list__item-row">
+  //       <h4 class="order-list__item-order">Order <span>${order.id}</span></h4>
+  //       <h3 class="order-list__item-ordered-date">${order.summary.createdAt}</h3>
+  //   </div>
+  //   <div class="order-list__item-row">
+  //       <h5 class="order-list__item-customer">${order.customerInfo.firstName} ${order.customerInfo.lastName}</h5>
+  //       <h5 class="order-list__item-order-time order-list__item-order-time--in-time">${order.summary.status}</h5>
+  //   </div>
+  //   <div class="order-list__item-row">
+  //       <h5 class="order-list__item-shipped">Shipped: <span>${order.summary.shippedAt}</span></h5>
+  //   </div>
+  // </div>`;    
 }
--- assets/js/modules/create-markup-all-orders-in-list.js
+++ assets/js/modules/create-markup-all-orders-in-list.js
@@ -6,15 +6,22 @@ export async function createMarkupAllOrdersInList() {
   let markup = "";
   const url = "http://localhost:3000/api/Orders";
 
+  // fetch(url)
+  // .then(res => console.log(res.json()))
+
   const res = await fetch(url);
   const data = await res.json();
   await (() => {
+    console.log(data);
     numberOfOrders.innerHTML = data.length;  
 
     if (!data.length) {
       markup = `<div class="no-orders">No orders available</div>`;
     }
-    data.forEach(order => markup += getMarkupOrderInList(order));
+    data.forEach(order => {
+      console.log(order);
+      markup += getMarkupOrderInList(order)
+    } );
 
     orderListMain.innerHTML = markup;
   })();
