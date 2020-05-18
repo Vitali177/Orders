308be4ace23ffa47b87edda6f618a324593e91a6
Vitali <vskrabatun@gmail.com>
2020-05-18 16:13:13 +0300

    feat: add error handling, if user didn't fill all inputs to create product

--- public/js/modules/create-product.js
+++ public/js/modules/create-product.js
@@ -2,6 +2,10 @@ export async function createProduct() {
   const productName = document.querySelector(".pop-up-form .input-name").value;
   const price = document.querySelector(".pop-up-form .input-price").value;
 
+  if ((productName || price) === "") {
+    return; // if user didn't fill all inputs
+  }
+
   const product = {productName, price};
   const url = `${window.location.origin}/api/OrderProducts`;
 
